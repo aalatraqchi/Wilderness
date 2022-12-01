830c988dbd33f494b3e5617344e9fe9cf02a3cfe
Ashraf Al-Atraqchi <ashraf.alatraqchi@gmail.com>
2022-12-01 15:52:19 -0500

    New health display
    Added new player health indicator on the screen that updates as the player health updates when getting damaged or respawning with full health.

--- gui.lua
+++ gui.lua
@@ -1,10 +1,15 @@
+love.graphics.setDefaultFilter("nearest", "nearest")
 local Player = require('player')
 local GUI = {}
 
 function GUI:load()
     self.health = {}
-
-
+    self.health.image = love.graphics.newImage('assets/heart.png')
+    self.health.width = self.health.image:getWidth()
+    self.health.height = self.health.image:getHeight()
+    self.health.scale = 0.1
+    self.health.x = 20
+    self.health.y = 10
 end
 
 function GUI:update(dt)
@@ -12,7 +17,19 @@ function GUI:update(dt)
 end
 
 function GUI:draw()
+    self:displayHealth()
+end
 
+function GUI:displayHealth()
+    love.graphics.setColor(0, 0, 0, 0.5)
+    love.graphics.draw(self.health.image, self.health.x + 2, self.health.y + 2, 0, self.health.scale, self.health.scale)
+    love.graphics.setColor(1, 1, 1, 1)
+    love.graphics.draw(self.health.image, self.health.x, self.health.y, 0, self.health.scale, self.health.scale)
+
+
+    local x = self.health.x + self.health.width * self.health.scale
+    local y = self.health.y + self.health.height * 0.2 * self.health.scale
+    love.graphics.print(" : " .. Player.health.current, x, y)
 end
 
 return GUI
